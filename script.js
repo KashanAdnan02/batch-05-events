f6681a1368305a3c04db65ad5c0c225b74c60403
Almiraadnan <almiraadnan34@gmail.com>
2025-04-28 19:02:22 +0500

    some-code

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-function showTime() {
-    const date = new Date()
-    console.log(date.getHours());
-}
+// function showTime() {
+//     const date = new Date()
+//     console.log(date.getHours());
+// }
 
 // showTime() // call
+let input = document.getElementById("input")
+
+console.log(input);
+
+
+function typeNumber(num) {
+    console.log(num);
+
+    console.log("hello Working");
+    input.value += num
+}
+
+
+function calculate() {
+    let input = document.getElementById("input")
+    // console.log();   
+    input.value = eval(input.value)
+}
+
+// eval
